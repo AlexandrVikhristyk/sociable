fa24e8852bfc4b8e763a78436f03a4e56111bf2b
BACKEND-STAR <alexvikhristyk@gmail.com>
2019-08-04 19:17:29 +0300

    add format view

--- src/main/resources/static/js/generate.js
+++ src/main/resources/static/js/generate.js
@@ -2,25 +2,19 @@
 
 let showList = [];
 
-(function startApp() {
-	setTimeout(() => {
-		getData("http://localhost:8080/message");
-	}, 2000);
-})();
-
 function getData(from) {
 	let xhr = new XMLHttpRequest();
-	xhr.open("GET", from, true);
+	xhr.open("GET", from, false);
 
 	xhr.setRequestHeader('Content-Type', 'application/json');
 
 	xhr.onload = function() {
-		if(xhr.status == 200) {
+		if(xhr.status === 200) {
 			showList = JSON.parse(xhr.response);
 		} else{
 			alert( 'Ошибка: ' + xhr.status);
 		}
-	}
+	};
 
 	xhr.onerror = function() {
 		alert("Request failed");
@@ -28,20 +22,24 @@ function getData(from) {
 	xhr.send();
 }
 
+getData("http://localhost:8080/message");
 
 let list = Object.create(HTMLElement.prototype);
 
 list.createdCallback = function() {
-	let crUl = document.createElement("ul").classList.add("list_wrapper");
+	console.log(showList);
+	let crUl = document.createElement("ul");
+	crUl.classList.add("list-component__wrapper");
 	let fragment = new DocumentFragment();
 
-	for(let i = 0; i <= showList.length; i++) {
-		let crI = document.createElement("li").classList.add("list_wrapper__item");
-		crI.innerHTML = `Id - ${showList[i]['id']} | Text - ${showList[i]['text']} | Date - ${showList[i]['creationDate']}`;
-		fragment.append(crI);	
+	for(let i = 0; i < showList.length; i++) {
+		let crI = document.createElement("li");
+		crI.classList.add("list-component__wrapper__item");
+		crI.innerHTML = `<i>${showList[i].id}</i> - ${showList[i].text}`;
+		fragment.append(crI);
 	}
 	crUl.append(fragment);
-	this.innerHTML = crUl;
+	this.append(crUl);
 }
 
 let listComponent = document.registerElement("list-component",{
