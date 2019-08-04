8e134a46bcdb18ababc9b169e9b306018be308c8
ValentunSolodun <valentun.solodun@gmail.com>
2019-08-04 17:13:14 +0300

    Configure request

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,19 @@
 (function startApp() {
 	setTimeout(() => {
-		alert("TEST");
+		getData("localhost:8080/message");
 	}, 2000);
 })();
+
+function getData(from) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET","from", false);
+
+	if(xhr.status == 200 && xhr.readyState == 4) {
+		let value = JSON.parse(xhr.responseText);
+		console.log(value);
+	};
+	xhr.onerror = function() {
+		alert("Request failed");
+	};
+	xhr.send();
+}
