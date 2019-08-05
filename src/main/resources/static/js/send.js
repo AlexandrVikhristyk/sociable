5f00b215f980b21775a6f0748f229b87e2e60a58
ValentunSolodun <valentun.solodun@gmail.com>
2019-08-06 00:07:00 +0300

    fix someone...

--- src/main/resources/static/js/send.js
+++ src/main/resources/static/js/send.js
@@ -108,4 +108,23 @@
 		return false;
 	};
 
+	let xhr2 = new XMLHttpRequest();
+	xhr2.open("GET", "http://localhost:8080/message", false);
+	xhr2.setRequestHeader('Content-Type', 'application/json');
+
+	xhr2.onload = function() {
+		if(xhr2.status === 200) {
+			document.querySelectorAll(".list-component__wrapper")[0].innerHTML = JSON.parse(xhr2.response);
+		} else{
+			alert( 'Ошибка: ' + xhr2.status);
+		}
+	};
+
+	xhr2.onerror = function() {
+		alert("Error");
+	};
+	xhr2.send();
+
+
+
 })();
