b5ad047f50efed5f952dfcae7473b10a5442d2c6
ValentunSolodun <valentun.solodun@gmail.com>
2019-08-05 19:57:40 +0300

    create delete method

--- src/main/resources/static/js/generate.js
+++ src/main/resources/static/js/generate.js
@@ -1,33 +1,79 @@
 (function() {
 
-let showList = [];
+let showList = [
+	{id: "1", text: "TEST"},
+	{id: "2", text: "TEST"},
+	{id: "3", text: "TEST"}
+];
 
-function getData(from) {
-	let xhr = new XMLHttpRequest();
-	xhr.open("GET", from, false);
+function getData(method, from){
 
-	xhr.setRequestHeader('Content-Type', 'application/json');
+	switch(method) {
 
-	xhr.onload = function() {
-		if(xhr.status === 200) {
-			showList = JSON.parse(xhr.response);
-		} else{
-			alert( 'Ошибка: ' + xhr.status);
-		}
-	};
+		case "GET":
+		 	return new Promise(function(resolve, reject) {
+				let xhr = new XMLHttpRequest();
+				xhr.open("GET", from, false);
 
-	xhr.onerror = function() {
-		alert("Request failed");
-	};
-	xhr.send();
-}
+				xhr.setRequestHeader('Content-Type', 'application/json');
 
-getData("http://localhost:8080/message");
+				xhr.onload = function() {
+					if(xhr.status === 200) {
+						showList = JSON.parse(xhr.response);
+						resolve("successful");
+					} else{
+						alert( 'Ошибка: ' + xhr.status);
+					}
+				};
+
+				xhr.onerror = function() {
+					reject();
+				};
+				xhr.send();
+			});
+		 break;
+		 case "DELETE": 
+			 return new Promise(function(resolve, reject) {
+				let xhr = new XMLHttpRequest();
+				xhr.open("DELETE", from, false);
+				xhr.send();
+			 });
+		 break;
+
+	}
+
+};
+
+getData("GET", "http://localhost:8080/message").then(
+	result => {
+		alert(result);
+	},
+	error => {
+		alert(error);
+	}
+)
+// ("http://localhost:8080/message");
 
 let list = Object.create(HTMLElement.prototype);
 
 list.createdCallback = function() {
-	console.log(showList);
+
+	function actnDel(target) {
+		return function() {
+			target.addEventListener("click", event => {
+				deleteProjectFromBase(event.target.dataset.stuffIdDelete);
+			});
+		}
+	}
+
+	function actnUpd(target) {
+		return function() {
+			target.addEventListener("click", event => {
+				updateProjectFromBase();
+			});
+		}
+	}
+
 	let crUl = document.createElement("ul");
 	crUl.classList.add("list-component__wrapper");
 	let fragment = new DocumentFragment();
@@ -35,10 +81,13 @@ list.createdCallback = function() {
 	for(let i = 0; i < showList.length; i++) {
 		let crI = document.createElement("li");
 
-		/////////////////////////////////////////
+		/////////////////////////////////////////dev vladoss
 	
 		let buttonDelete = document.createElement("button");
+		buttonDelete.dataset.stuffIdDelete = showList[i].id;
+		actnDel(buttonDelete)();
 		let buttonUpdate = document.createElement("button");
+		actnUpd(buttonUpdate)();
 		buttonDelete.append(document.createTextNode("Delete"));
 		buttonUpdate.append(document.createTextNode("Update"));
 		crI.append(document.createTextNode(`${showList[i].id} - ${showList[i].text}`));
@@ -47,9 +96,8 @@ list.createdCallback = function() {
 
 		/////////////////////////////////////////
 
-
 		crI.classList.add("list-component__wrapper__item");
-		// crI.innerHTML = `<i>${showList[i].id}</i> - ${showList[i].text}`;
+		crI.dataset.stuffId = showList[i].id;
 		fragment.append(crI);
 	}
 	crUl.append(fragment);
@@ -59,6 +107,23 @@ list.createdCallback = function() {
 let listComponent = document.registerElement("list-component",{
 	prototype: list
 });
+
+function deleteProjectFromBase(id) {
+	// let xhr = new XMLHttpRequest();
+	
+	getData("DELETE", "http://localhost:8080/message/1").then(
+		result => {
+			alert(result);
+		},
+		error => {
+			alert(error);
+		}
+	)
+}
+
+function updateProjectFromBase() {
+	alert("test");
+}
 
 
 })();
