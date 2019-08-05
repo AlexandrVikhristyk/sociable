d5d2b852e4f5faa312bd72d0a80c0f166a3e15df
VladosS1310 <vladik.prindyuk007@gmail.com>
2019-08-05 22:55:21 +0300

    add Login

--- src/main/resources/static/js/send.js
+++ src/main/resources/static/js/send.js
@@ -4,7 +4,9 @@
 
 	formComponent.createdCallback = function() {
 
-	let form = document.createElement("form");
+	let formReg = document.createElement("form");
+	let forTitle = document.createElement("p");
+	forTitle.innerHTML = "Form Registrtion";
 
 	let fragment = new DocumentFragment();
 		for (let i = 0; i < 4; i++) {
@@ -12,20 +14,24 @@
 			if(i == 0) {
 				crInput.setAttribute("type", "name");
 				crInput.setAttribute("name", "username");
+				crInput.setAttribute("placeholder", "Username");
 			} else if(i == 1) {
 				crInput.setAttribute("type", "password");
 				crInput.setAttribute("name", "hashPass");
+				crInput.setAttribute("placeholder", "Password");
 			} else if(i == 2) {
 				crInput.setAttribute("type", "email");
-				crInput.setAttribute("nmae", "email");
+				crInput.setAttribute("name", "email");
+				crInput.setAttribute("placeholder", "Example@gmail.com");
 			} else {
 				crInput.setAttribute("type", "submit");
 			}
 			fragment.append(crInput);
 		}
-		form.append(fragment);
-		form.setAttribute("name", "registerForm");
-		this.append(form);
+		formReg.append(fragment);
+		formReg.setAttribute("name", "registerForm");
+		// form.insertBefore(forTitle, crInput[0]);	
+		this.append(formReg);
 
 	let formRegist = document.forms.registerForm;
 	let valLogin = formRegist.elements.username;
@@ -66,6 +72,28 @@
 			let obj = ({
 				id: null,
 				text: valInput.value
+			});
+			let json = JSON.stringify(obj);
+			let xhr = new XMLHttpRequest();
+			console.log(json);
+			xhr.open("POST", "http://localhost:8080/message");
+			xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+			xhr.send(json);
+		}
+		return false;
+	};
+
+	let formLogin = document.forms.formLogin;
+	let valueLogin = formLogin.elements.login;
+	let valuePassword = formLogin.elements.password;
+
+	formLogin.onsubmit = function() {
+		if(!valueLogin.value || !valuePassword.value) {
+			alert("Error");
+		} else {
+			let obj = ({
+				login: valueLogin.value,
+				password: valuePassword.value
 			});
 			let json = JSON.stringify(obj);
 			let xhr = new XMLHttpRequest();
