526870bfe04c47435bc110645a0e3f2c3ce64617
BACKEND-STAR <alexvikhristyk@gmail.com>
2019-08-07 02:05:38 +0300

    delete mainController and replaced by app config

---
+++ src/main/java/com/example/demo/configs/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/testSocket").setViewName("index.html");
+        registry.addViewController("/").setViewName("templates/index.html");
+    }
+
+}
