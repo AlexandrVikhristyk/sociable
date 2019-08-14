2edb4e6c1464108ee1558c2a0c99f29b1fb54edc
VladosS1310 <vladik.prindyuk007@gmail.com>
2019-08-14 15:12:49 +0300

    upgrade Dropdown menu

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -8,6 +8,10 @@ window.onload = function() {
 	let local = localStorage.getItem('login');
 	let user = document.getElementById("user");
 	let span = document.createElement("span");
+	let img = document.createElement('img');
+	img.setAttribute("class", "dropdownArrow");
+	img.setAttribute("src", "../css/img/dropdownArrow.svg");
 	span.append(document.createTextNode(`Welcome: ${local}`));
 	user.append(span);
+	span.append(img);
 };
